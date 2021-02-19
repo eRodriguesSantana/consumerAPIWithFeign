81503f183ed371e45e48fff309bd45dd22137968
eort <eduardo.santana@gft.com>
2021-02-19 14:16:23 -0300

    Criação de interface de acesso ao servidor

--- src/main/java/com/erodriguessantana/consumerAPI/service/IBookstoremanager.java
+++ src/main/java/com/erodriguessantana/consumerAPI/service/IBookstoremanager.java
@@ -1,14 +1,14 @@
 package com.erodriguessantana.consumerAPI.service;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.erodriguessantana.consumerAPI.entity.Book;
 
-@Configurable
-@FeignClient(url = "http://localhost:8080/api/v1/books/", name = "bookstoremanager")
+@Component
+@FeignClient(url = "localhost:8080", path = "/api/v1/books/", name = "bookstoremanager")
 public interface IBookstoremanager {
 
 	@GetMapping("/{id}")
